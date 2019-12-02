bc1ee9c79f86dba2127b4a550c573e1c8152931b
Hugo SABOURIN <hsabouri@student.42.fr>
2019-12-02 14:49:39 +0100

    working turing machine

--- app/Main.hs
+++ app/Main.hs
@@ -10,23 +10,23 @@ import Text.Printf (printf)
 import Machine ( Machine(..), encode, eitherDecode )
 import Execute ( MachineState(..), step )
 
-execute :: MachineState -> String -> String
-execute m acc
+execute :: MachineState -> String -> Int -> String
+execute m acc i
     | elem (state m) (finals $ machine m) =
         acc
-execute m acc =
-    let m = step m in
-    execute m $ printf "%s\n%s" acc $ input m
-
--- solve infinit loop
+    | i == 0 =
+        acc
+execute m acc i =
+    let new_m = step m in
+    let new_acc = printf "%s\n%s" acc $ input m in
+    execute new_m new_acc (i - 1)
 
 ft_turing :: String -> Either String Machine -> String
 ft_turing _ (Left s) = s
 ft_turing tape (Right m) =
     let first = tape in
     let machine_state = MachineState 0 (initial m) tape m in
-    printf "%s\n%s" first $ execute machine_state ""
-    
+    printf "%s\n%s" first $ execute machine_state "" 1000
 
 usage :: IO ()
 usage = putStrLn "usage: ft_turing [-h] jsonfile input\n\npositional arguments:\n  jsonfile\t\tjson description of the machine\n  input\t\t\tinput of the machine\n\noptional arguments:\n  -h, --help\t\tshow this help message and exit"
--- src/Execute.hs
+++ src/Execute.hs
@@ -16,10 +16,11 @@ data MachineState = MachineState {
     machine     :: Machine
 }
 
+enumerate :: [b] -> [(Int, b)]
 enumerate = zip [0..]
 
 change_by_id :: Int -> Letter -> (Int, Char) -> Char
-change_by_id id new (i, a)
+change_by_id id new (i, _)
     | id == i =
         head new
 change_by_id _ _ (_, a) = a
@@ -29,12 +30,9 @@ change_pos LEFT i = i - 1
 change_pos RIGHT i = i + 1
 
 itemOf :: Int -> [a] -> Maybe a
-itemOf _ [] =
-    Nothing
-itemOf 0 (head:_) =
-    Just head
-itemOf i (head:tail) =
-    itemOf (i - 1) tail
+itemOf _ [] = Nothing
+itemOf 0 (head : _) = Just head
+itemOf i (_ : tail) = itemOf (i - 1) tail
 
 execute :: MachineState -> Maybe Transition -> MachineState
 execute machine_state (Just transition) =
@@ -43,7 +41,6 @@ execute machine_state (Just transition) =
     let new_pos = change_pos (action transition) (position machine_state) in
     let new_state = to_state transition in
     MachineState new_pos new_state new_input (machine machine_state)
-
 execute machine_state Nothing = machine_state
 
 default_list :: Maybe [a] -> [a]
@@ -56,6 +53,7 @@ default_read (Nothing) def = def
 
 step :: MachineState -> MachineState
 step machine_state
+    -- current state is part of [finals]
     | elem (state machine_state) $ finals $ machine machine_state =
         machine_state
 step machine_state =
